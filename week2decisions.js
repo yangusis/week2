465273acae4e8786b14b8a6da67bfba7058ffb5b
yangusis <yangusis@gmail.com>
2022-08-26 19:59:14 -0700

    finished decisions1 problem 1

---
+++ week2decisions.js
@@ -0,0 +1,13 @@
+/*
+  Create a variable called 'lovesCode' and set it equal to true. 
+  Check to see if 'lovesCode' is equal to true or false. 
+  If it is true, console log "I love to code!"
+  If it is not, console log "Coding has its challenges."
+*/
+let lovesCode = true
+
+if(lovesCode) {
+    console.log("I love to code!")
+} else {
+    console.log("Coding has its challenges.")
+}
